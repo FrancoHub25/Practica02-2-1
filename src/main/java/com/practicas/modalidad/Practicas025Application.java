748a779af3c2103a9dd83c58c3b82b52bda97095
FrancoHub25 <francoretamozo25@gmail.com>
2023-11-26 18:00:53 -0500

    Hello

---
+++ src/main/java/com/practicas/modalidad/Practicas025Application.java
@@ -0,0 +1,13 @@
+package com.practicas.modalidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practicas025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practicas025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/practicas/modalidad/Practicas025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicas.modalidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practicas025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
